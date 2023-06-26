6b95c0662ab7a410ca5916eca0bafffe2ab3852f
leandro-prevideli <137085512+leandro-prevideli@users.noreply.github.com>
2023-06-26 15:57:20 -0300

    Update and rename index.js to main.js

---
+++ main.js
@@ -0,0 +1,34 @@
+
+// CARDS
+document.addEventListener('DOMContentLoaded', function() {
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  });
+
+  // DISCOVER NOW
+  function reveal() {
+    var reveals = document.querySelectorAll(".discover, .activities-panel");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
